773ae066721fab597c32248db01f614bdc1957a0
Gabrielneves05 <gabrielneves05@outlook.com>
2025-05-15 02:44:48 -0300

    chore: project connection to the database

--- config/db.js
+++ config/db.js
@@ -1,3 +1,19 @@
 const mongoose = require("mongoose");
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
 
-// connect to mongodb
+const conn = async () => {
+    try {
+        const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster-vinx.cpyaaok.mongodb.net/?retryWrites=true&w=majority&appName=cluster-vinx`);
+
+        console.log('Connected to MongoDB!');
+
+        return dbConn;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+conn();
+
+module.exports = conn;

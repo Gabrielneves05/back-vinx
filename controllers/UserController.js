f0f2f6a20ebdcda53985a34a44338e1bb856df7a
Gabrielneves05 <gabrielneves05@outlook.com>
2025-05-20 12:50:42 -0300

    feat: implementation of functionalities and validations in user registration

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -14,7 +14,39 @@ const generateToken = (id) => {
 
 // Register an user and sign in
 const register = async(req, res) => {
-    res.send("Registro");
+    const { name, email, password } = req.body;
+
+    // check if user already exists
+    const user = await User.findOne({ email });
+
+    if(user) {
+        res.status(422).json({errors: ["Please use another email."]});
+
+        return;
+    }
+
+    // generate hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    // create user
+    const newUser = await User.create({
+        name,
+        email,
+        password: hashPassword,
+    });
+
+    // if user was created successfully, return the token
+    if(!newUser) {
+        res.status(422).json({errors: ["There was an error, please try again later."]});
+
+        return;
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id),
+    });
 }
 
 module.exports = {

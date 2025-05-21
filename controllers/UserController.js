fe261edc67e73078aab64e9036c03d8b253b05e0
Gabrielneves05 <gabrielneves05@outlook.com>
2025-05-21 18:31:11 -0300

    feat: add user login functionality / create middleware for authentication validation / get authenticated user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -13,7 +13,7 @@ const generateToken = (id) => {
 }
 
 // Register an user and sign in
-const register = async(req, res) => {
+const register = async (req, res) => {
     const { name, email, password } = req.body;
 
     // check if user already exists
@@ -50,11 +50,42 @@ const register = async(req, res) => {
 }
 
 // Sign user in
-const login = (req, res) => {
-    res.send("Login");
+const login = async (req, res) => {
+    const {email, password} = req.body;
+
+    const user = await User.findOne({email});
+
+    // check if user exists
+    if(!user) {
+        res.status(404).json({errors: ["User not found."]});
+
+        return;
+    }
+
+    // check if password matches
+    if(!(await bcrypt.compare(password, user.password))) {
+        res.status(422).json({errors: ["Incorrect password."]});
+
+        return;
+    }
+
+    // Return user with token
+    res.status(201).json({
+        _id: user._id,
+        profileImage: user.profileImage,
+        token: generateToken(user._id),
+    });
+}
+
+// Get current logged in user
+const getCurrentUser = async (req, res) => {
+    const user = req.user;
+
+    res.status(200).json(user);
 }
 
 module.exports = {
     register,
     login,
+    getCurrentUser,
 }

696ac767500c9bc311b99744ecd3f2e60eecd8df
Gabriel Neves <129914892+Gabrielneves05@users.noreply.github.com>
2026-02-02 16:23:48 -0300

    Refactor MongoDB connection in db.js
    Refactor MongoDB connection logic to improve error handling and connection string.

--- config/db.js
+++ config/db.js
@@ -1,19 +1,18 @@
 const mongoose = require("mongoose");
-
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
 
 const conn = async () => {
-  try {
-    await mongoose.connect(
-      `mongodb+srv://${dbUser}:${dbPassword}@cluster-vinx.cpyaaok.mongodb.net/vinx?retryWrites=true&w=majority&appName=cluster-vinx`
-    );
+    try {
+        const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster-vinx.cpyaaok.mongodb.net/?retryWrites=true&w=majority&appName=cluster-vinx`);
 
-    console.log("Connected to MongoDB!");
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-  }
-};
+        console.log('Connected to MongoDB!');
+
+        return dbConn;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 conn();
 

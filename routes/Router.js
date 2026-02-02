5fdbe3ccbb6a21312d1a6ae9ecf05caf31cc3636
Gabriel Neves <129914892+Gabrielneves05@users.noreply.github.com>
2026-02-02 15:31:57 -0300

    Refactor router initialization to use express.Router()

--- routes/Router.js
+++ routes/Router.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const router = express();
+
+const router = express.Router();
 
 router.use("/api/users", require("./UserRoutes"));
 router.use("/api/photos", require("./PhotoRoutes"));
 
-// test
+// rota de teste
 router.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
 
 module.exports = router;

20d4885c65915bc8c177660d191c99243712f523
Gabriel Neves <129914892+Gabrielneves05@users.noreply.github.com>
2026-02-02 16:24:23 -0300

    Refactor CORS and DB connection setup in app.js

--- app.js
+++ app.js
@@ -4,33 +4,29 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
-// DB connection
-require("./config/db");
-
 const port = process.env.PORT || 3333;
 
 const app = express();
 
 // configs JSON and form data response
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Resolve CORS (liberado para produção e dev)
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
+// Resolve CORS
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
 // Upload directory images
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+// DB connection
+require("./config/db.js");
+
 // routes
-const router = require("./routes/Router");
+const router = require("./routes/Router.js");
 
 app.use(router);
 
 app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
+    console.log(`App is running on port ${port}`);
 });
